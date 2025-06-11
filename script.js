06bdbbb20088f35daa556c6905e4c894dd096df1
andtmal <208543898+andtmal@users.noreply.github.com>
2025-06-11 13:03:51 -0500

    Add square event listener and color picking caps.
    User can pick a color and draw with it by hovering over each square.

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ document.querySelector("#create-grid-button").addEventListener("click", createCa
 
 function createCanvas() {
   const canvas = document.querySelector("#canvas");
-  const size = document.querySelector("#grid-input").value;
+  const size = document.querySelector("#grid-input");
+
   canvas.innerHTML = "";
 
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size.value; i++) {
     const row = document.createElement("row")
-    row.id = "row";
+    row.classList.add("row");
 
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < size.value; j++) {
       const square = document.createElement("div");
-      square.id = "square";
+      square.classList.add("square");
       row.appendChild(square);
+
+      square.addEventListener("mouseover", () => {
+        const color = document.querySelector("#color-picker");
+        square.style.backgroundColor = color.value;
+      });
     }
     canvas.appendChild(row);
   }
+  size.value = "";
 }
 
+document.querySelector("#reset-button").addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+
+  squares.forEach(square => {
+      square.style.backgroundColor = "white";
+  });
+});
